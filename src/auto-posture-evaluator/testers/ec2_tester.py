bd6ce706d7737aa34db585e1c30157f12b6e677a
sleepy0owl <souravcristiano502@gmail.com>
2021-12-02 18:06:29 +0530

    Add helper methods to reduce complexity

--- src/auto-posture-evaluator/testers/ec2_tester.py
+++ src/auto-posture-evaluator/testers/ec2_tester.py
@@ -20,41 +20,50 @@ class Tester(interfaces.TesterInterface):
         return 'aws'
 
     def run_tests(self) -> list:
+        all_inbound_permissions = self._get_all_inbound_permissions(self.instances)
+        all_instance_ids = self._get_all_instance_ids(self.instances)
         return \
-            self.get_inbound_http_access(self.instances) + \
-            self.get_inbound_https_access(self.instances) + \
-            self.get_inbound_mongodb_access(self.instances) + \
-            self.get_inbound_mysql_access(self.instances) + \
-            self.get_inbound_mssql_access(self.instances) + \
-            self.get_inbound_ssh_access(self.instances) + \
-            self.get_inbound_rdp_access(self.instances) + \
-            self.get_inbound_postgresql_access(self.instances) + \
-            self.get_inbound_tcp_netbios_access(self.instances)
+            self.get_inbound_http_access(all_inbound_permissions, all_instance_ids) + \
+            self.get_inbound_https_access(all_inbound_permissions, all_instance_ids) + \
+            self.get_inbound_mongodb_access(all_inbound_permissions, all_instance_ids) + \
+            self.get_inbound_mysql_access(all_inbound_permissions, all_instance_ids) + \
+            self.get_inbound_mssql_access(all_inbound_permissions, all_instance_ids) + \
+            self.get_inbound_ssh_access(all_inbound_permissions, all_instance_ids) + \
+            self.get_inbound_rdp_access(all_inbound_permissions, all_instance_ids) + \
+            self.get_inbound_postgresql_access(all_inbound_permissions, all_instance_ids) + \
+            self.get_inbound_tcp_netbios_access(all_inbound_permissions, all_instance_ids)
     
-    def _get_inbound_port_access(self, instances, target_port, test_name, protocol="tcp"):
-        result = []
+    def _get_all_instance_ids(self, instances):
+        return list(map(lambda i: i.id, list(instances)))
+
+    def _get_all_inbound_permissions(self, instances):
+        all_inbound_permissions = []
         for instance in instances:
-            # get security group of that instance 
             security_groups = instance.security_groups
-            all_inbound_permissions = []
             for security_group in security_groups:
                 inbound_permissions = self.aws_ec2_resource.SecurityGroup(security_group['GroupId']).ip_permissions
                 for i in inbound_permissions:
+                    i['instance'] = instance
                     all_inbound_permissions.append(i)
+        return all_inbound_permissions
 
-            https_port_access = list(filter(lambda permission: permission['FromPort'] == target_port and permission['ToPort'] == target_port and permission['IpProtocol'] == protocol, all_inbound_permissions))
-            if len(https_port_access) == 0:
-                result.append({
-                    "user": self.user_id,
-                    "account_arn": self.account_arn,
-                    "account": self.account_id,
-                    "timestamp": time.time(),
-                    "item": instance.id,
-                    "item_type": "ec2",
-                    "test_name": test_name
-                })
-            else:
-                pass
+    def _get_inbound_port_access(self, instance_ids, all_inbound_permissions, target_port, test_name, protocol="tcp"):
+        result = []
+        instances = list(map(lambda i: i['instance'].id, list(filter(lambda permission: permission['FromPort'] == target_port and permission['ToPort'] == target_port and permission['IpProtocol'] == protocol, all_inbound_permissions))))
+        all_instance_ids = set(instance_ids)
+        filtered_instance_ids = set(instances)
+        restricted_instances = all_instance_ids.difference(filtered_instance_ids)
+
+        for i in restricted_instances:
+            result.append({
+               "user": self.user_id,
+                "account_arn": self.account_arn,
+                "account": self.account_id,
+                "timestamp": time.time(),
+                "item": i,
+                "item_type": "ec2",
+                "test_name": test_name 
+            })
         if len(result) == 0:
             result.append({
                 "user": self.user_id,
@@ -67,38 +76,38 @@ class Tester(interfaces.TesterInterface):
             })
         return result
 
-    def get_inbound_http_access(self, instances):
+    def get_inbound_http_access(self, all_inbound_permissions, all_instance_ids):
         test_name = "ec2_inbound_http_access_restricted"
-        return self._get_inbound_port_access(instances, 80, test_name)
+        return self._get_inbound_port_access(all_instance_ids, all_inbound_permissions, 80, test_name)
 
-    def get_inbound_https_access(self, instances):
+    def get_inbound_https_access(self, all_inbound_permissions, all_instance_ids):
         test_name = "ec2_inbound_https_access_restricted"
-        return self._get_inbound_port_access(instances, 443, test_name)
+        return self._get_inbound_port_access(all_instance_ids, all_inbound_permissions, 443, test_name)
     
-    def get_inbound_mongodb_access(self, instances):
+    def get_inbound_mongodb_access(self, all_inbound_permissions, all_instance_ids):
         test_name = "ec2_inbound_mongodb_access_restricted"
-        return self._get_inbound_port_access(instances, 27017, test_name)
+        return self._get_inbound_port_access(all_instance_ids, all_inbound_permissions, 27017, test_name)
     
-    def get_inbound_mysql_access(self, instances):
+    def get_inbound_mysql_access(self, all_inbound_permissions, all_instance_ids):
         test_name = "ec2_inbound_mysql_access_restricted"
-        return self._get_inbound_port_access(instances, 3306, test_name)
+        return self._get_inbound_port_access(all_instance_ids, all_inbound_permissions, 3306, test_name)
     
-    def get_inbound_mssql_access(self, instances):
+    def get_inbound_mssql_access(self, all_inbound_permissions, all_instance_ids):
         test_name = "ec2_inbound_mssql_access_restricted"
-        return self._get_inbound_port_access(instances, 1433, test_name)
+        return self._get_inbound_port_access(all_instance_ids, all_inbound_permissions, 1433, test_name)
 
-    def get_inbound_ssh_access(self, instances):
+    def get_inbound_ssh_access(self, all_inbound_permissions, all_instance_ids):
         test_name = "ec2_inbound_ssh_access_restricted"
-        return self._get_inbound_port_access(instances, 22, test_name)
+        return self._get_inbound_port_access(all_instance_ids, all_inbound_permissions, 22, test_name)
 
-    def get_inbound_rdp_access(self, instances):
+    def get_inbound_rdp_access(self, all_inbound_permissions, all_instance_ids):
         test_name = "ec2_inbound_rdp_access_restricted"
-        return self._get_inbound_port_access(instances, 3389, test_name)
+        return self._get_inbound_port_access(all_instance_ids, all_inbound_permissions, 3389, test_name)
     
-    def get_inbound_postgresql_access(self, instances):
+    def get_inbound_postgresql_access(self, all_inbound_permissions, all_instance_ids):
         test_name = "ec2_inbound_postgresql_access_restricted"
-        return self._get_inbound_port_access(instances, 5432, test_name)
+        return self._get_inbound_port_access(all_instance_ids, all_inbound_permissions, 5432, test_name)
 
-    def get_inbound_tcp_netbios_access(self, instances):
+    def get_inbound_tcp_netbios_access(self, all_inbound_permissions, all_instance_ids):
         test_name = "ec2_inbound_tcp_netbios_access_restricted"
-        return self._get_inbound_port_access(instances, 139, test_name)
+        return self._get_inbound_port_access(all_instance_ids, all_inbound_permissions, 139, test_name)
