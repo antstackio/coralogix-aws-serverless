f959576faa6a1c685019c644cbc40241a7b464a3
sleepy0owl <souravcristiano502@gmail.com>
2021-12-29 01:38:26 +0530

    Add ELB(classic) generating access log

--- src/auto-posture-evaluator/testers/elb_tester.py
+++ src/auto-posture-evaluator/testers/elb_tester.py
@@ -63,53 +63,6 @@ class Tester(interfaces.TesterInterface):
                 })
         
         return result
-
-    def get_elbv2_generating_access_log(self) -> List:
-        test_name = "elbv2_is_generating_access_logs"
-        result = []
-        elbs = self.elbsv2
-
-        for elb in elbs:
-            elb_arn = elb['LoadBalancerArn']
-            elb_attributes = self.aws_elbsv2_client.describe_load_balancer_attributes(LoadBalancerArn=elb_arn)
-
-            attributes = elb_attributes['Attributes']
-            for i in attributes:
-                if i['Key'] == 'access_logs.s3.enabled':
-                    if i['Value'] == 'false':
-                        result.append({
-                            "user": self.user_id,
-                            "account_arn": self.account_arn,
-                            "account": self.account_id,
-                            "timestamp": time.time(),
-                            "item": elb_arn,
-                            "item_type": "aws_elbv2",
-                            "test_name": test_name,
-                            "test_result": "issue_found"
-                        })
-                    else:
-                        result.append({
-                            "user": self.user_id,
-                            "account_arn": self.account_arn,
-                            "account": self.account_id,
-                            "timestamp": time.time(),
-                            "item": elb_arn,
-                            "item_type": "aws_elbv2",
-                            "test_name": test_name,
-                            "test_result": "no_issue_found"
-                        })
-                else:
-                    result.append({
-                        "user": self.user_id,
-                        "account_arn": self.account_arn,
-                        "account": self.account_id,
-                        "timestamp": time.time(),
-                        "item": elb_arn,
-                        "item_type": "aws_elbv2",
-                        "test_name": test_name,
-                        "test_result": "no_issue_found"
-                    })
-        return result
     
     def get_elb_generating_access_log(self) -> List:
         elbs = self.elbs
@@ -145,46 +98,3 @@ class Tester(interfaces.TesterInterface):
                 })
         
         return result
-
-    def get_alb_using_secure_listener(self) -> List:
-        test_name = "alb_is_using_secure_listeners"
-        elbs = self.elbs
-        result = []
-
-        for elb in elbs:
-            # check elbv2 type and only let ALB pass
-            if elb['Type'] == "application":
-                load_balancer_arn = elb['LoadBalancerArn']
-                listeners = self.aws_elbsv2_client.describe_listeners(LoadBalancerArn=load_balancer_arn)
-                secure_listener_count = 0
-                for listener in listeners:
-                    if listener['Protocol'] == "HTTPS":
-                        secure_listener_count += 1
-                
-                if secure_listener_count == len(listener):
-                    result.append({
-                        "user": self.user_id,
-                        "account_arn": self.account_arn,
-                        "account": self.account_id,
-                        "timestamp": time.time(),
-                        "item": load_balancer_arn,
-                        "item_type": "aws_elbv2",
-                        "test_name": test_name,
-                        "test_result": "no_issue_found"
-                    })
-                else:
-                    result.append({
-                        "user": self.user_id,
-                        "account_arn": self.account_arn,
-                        "account": self.account_id,
-                        "timestamp": time.time(),
-                        "item": load_balancer_arn,
-                        "item_type": "aws_elbv2",
-                        "test_name": test_name,
-                        "test_result": "issue_found"
-                    })
-            else:
-                continue
-        
-        return result
-    
