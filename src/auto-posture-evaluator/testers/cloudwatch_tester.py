fde06911c7b2be8c14bd044f35e70da2b6dae299
Vaibhav Gope <vaibhav.gope@antstack.io>
2022-03-09 18:07:39 +0530

    Add more cloudwatch checks and one cloudformation check for stack notifications

--- src/auto-posture-evaluator/testers/cloudwatch_tester.py
+++ src/auto-posture-evaluator/testers/cloudwatch_tester.py
@@ -1,10 +1,12 @@
 import time
 import interfaces
 import boto3
+import botocore.exceptions
 
 class Tester(interfaces.TesterInterface):
     def __init__(self):
         self.aws_cloudwatch_client = boto3.client('cloudwatch')
+        self.aws_cloudformation_client = boto3.client('cloudformation')
         self.cache = {}
         self.user_id = boto3.client('sts').get_caller_identity().get('UserId')
         self.account_arn = boto3.client('sts').get_caller_identity().get('Arn')
@@ -318,3 +320,119 @@ class Tester(interfaces.TesterInterface):
                 "test_name": test_name,
                 "test_result": "issue_found"
             }]
+    
+    def get_network_acl_changes_not_monitored(self):
+        test_name = "network_acl_changes_not_monitored"
+        alarms = self.aws_cloudwatch_client.describe_alarms_for_metric(MetricName='NetworkAclEventCount', Namespace='CloudTrailMetrics')
+        if len(alarms['MetricAlarms']) > 0:
+            return [{
+                "user": self.user_id,
+                "account_arn": self.account_arn,
+                "account": self.account_id,
+                "timestamp": time.time(),
+                "item_type": "cloudwatch_alarm",
+                "test_name": test_name,
+                "test_result": "no_issue_found"
+            }]
+        else:
+            return [{
+                "user": self.user_id,
+                "account_arn": self.account_arn,
+                "account": self.account_id,
+                "timestamp": time.time(),
+                "item_type": "cloudwatch_alarm",
+                "test_name": test_name,
+                "test_result": "issue_found"
+            }]
+    
+    def get_aws_config_configuration_changes_not_monitored(self):
+        test_name = "aws_config_configuration_changes_not_monitored"
+        alarms = self.aws_cloudwatch_client.describe_alarms_for_metric(MetricName='ConfigEventCount', Namespace='CloudTrailMetrics')
+        if len(alarms['MetricAlarms']) > 0:
+            return [{
+                "user": self.user_id,
+                "account_arn": self.account_arn,
+                "account": self.account_id,
+                "timestamp": time.time(),
+                "item_type": "cloudwatch_alarm",
+                "test_name": test_name,
+                "test_result": "no_issue_found"
+            }]
+        else:
+            return [{
+                "user": self.user_id,
+                "account_arn": self.account_arn,
+                "account": self.account_id,
+                "timestamp": time.time(),
+                "item_type": "cloudwatch_alarm",
+                "test_name": test_name,
+                "test_result": "issue_found"
+            }]
+
+    def get_iam_policy_changes_not_monitored(self):
+        test_name = "iam_policy_changes_not_monitored"
+        alarms = self.aws_cloudwatch_client.describe_alarms_for_metric(MetricName='IAMPolicyEventCount', Namespace='CloudTrailMetrics')
+        if len(alarms['MetricAlarms']) > 0:
+            return [{
+                "user": self.user_id,
+                "account_arn": self.account_arn,
+                "account": self.account_id,
+                "timestamp": time.time(),
+                "item_type": "cloudwatch_alarm",
+                "test_name": test_name,
+                "test_result": "no_issue_found"
+            }]
+        else:
+            return [{
+                "user": self.user_id,
+                "account_arn": self.account_arn,
+                "account": self.account_id,
+                "timestamp": time.time(),
+                "item_type": "cloudwatch_alarm",
+                "test_name": test_name,
+                "test_result": "issue_found"
+            }]
+
+    def get_enable_aws_cloudformation_stack_notifications(self):
+        test_name = "enable_aws_cloudformation_stack_notifications"
+        stacks = self.aws_cloudformation_client.list_stacks()
+        result = []
+        for stack in stacks['StackSummaries']:
+            stack_name = stack['StackName']
+            issue_detected = False
+            try:
+                stack_info = self.aws_cloudformation_client.describe_stacks(StackName=stack_name)
+                notif_arns = stack_info['Stacks'][0]['NotificationARNs']
+                if not notif_arns or len(notif_arns) == 0:
+                    issue_detected = True
+            except botocore.exceptions.ClientError as ex:
+                if ex.response['Error']['Code'] == 'ValidationError':
+                    issue_detected = True
+                else:
+                    raise ex
+            
+            if issue_detected:
+                result.append({
+                    "user": self.user_id,
+                    "account_arn": self.account_arn,
+                    "account": self.account_id,
+                    "timestamp": time.time(),
+                    "item": stack_name,
+                    "item_type": "cloudformation_stack",
+                    "test_name": test_name,
+                    "test_result": "issue_found"
+                })
+            else:
+                result.append({
+                    "user": self.user_id,
+                    "account_arn": self.account_arn,
+                    "account": self.account_id,
+                    "timestamp": time.time(),
+                    "item": stack_name,
+                    "item_type": "cloudformation_stack",
+                    "test_name": test_name,
+                    "test_result": "no_issue_found"
+                })
+
+        return result
+        
