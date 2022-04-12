7fc1d523d70c25ffdef04bcf35ae189980429251
Vaibhav Gope <vaibhav.gope@antstack.io>
2022-04-12 13:12:40 +0530

    Add get apps with no description check

--- src/auto-posture-evaluator/testers/slack_tester.py
+++ src/auto-posture-evaluator/testers/slack_tester.py
@@ -18,7 +18,8 @@ class Tester(interfaces.TesterInterface):
 
     def run_tests(self) -> list:
         return self.get_public_file_sharing_enabled() + \
-               self.get_apps_with_no_privacy_policy()
+               self.get_apps_with_no_privacy_policy() + \
+               self.get_apps_with_no_description()
 
     def get_public_file_sharing_enabled(self):
         test_name = "public_file_sharing_enabled"
@@ -51,6 +52,31 @@ class Tester(interfaces.TesterInterface):
         result = []
         for app in response['approved_apps']:
             if not app['app']['privacy_policy_url']:
+                result.append({
+                    "timestamp": time.time(),
+                    "account": self.team_id,
+                    "item": app["app"]["id"],
+                    "item_type": "app",
+                    "test_name": test_name,
+                    "test_result": "issue_found"
+                })
+            else:
+                result.append({
+                    "timestamp": time.time(),
+                    "account": self.team_id,
+                    "item": app["app"]["id"],
+                    "item_type": "app",
+                    "test_name": test_name,
+                    "test_result": "no_issue_found"
+                })
+        return result
+
+    def get_apps_with_no_description(self):
+        test_name = "apps_with_no_description"
+        response = self.slack_client.admin_apps_approved_list(team_id=self.team_id)
+        result = []
+        for app in response['approved_apps']:
+            if not app['app']['description']:
                 result.append({
                     "timestamp": time.time(),
                     "account": self.team_id,
