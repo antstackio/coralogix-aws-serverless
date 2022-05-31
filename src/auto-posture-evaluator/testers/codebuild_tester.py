3919cd50e670d669c4034e233bee4264cbf48210
sleepy0owl <souravcristiano502@gmail.com>
2022-05-31 12:41:46 +0530

    Fix artifact encryption disabled issue

--- src/auto-posture-evaluator/testers/codebuild_tester.py
+++ src/auto-posture-evaluator/testers/codebuild_tester.py
@@ -2,6 +2,7 @@ import time
 import boto3
 import interfaces
 
+
 class Tester(interfaces.TesterInterface):
     def __init__(self) -> None:
         self.user_id = boto3.client('sts').get_caller_identity().get('UserId')
@@ -9,17 +10,17 @@ class Tester(interfaces.TesterInterface):
         self.account_id = boto3.client('sts').get_caller_identity().get('Account')
         self.aws_codebuild_client = boto3.client('codebuild')
         self.codebuild_projects = self._get_all_codebuild_projects()
-    
+
     def declare_tested_provider(self) -> str:
         return "aws"
-    
+
     def declare_tested_service(self) -> str:
         return "codebuild"
-    
+
     def run_tests(self) -> list:
         return \
             self.codebuild_project_build_artifacts_should_be_encrypted()
-    
+
     def _get_all_codebuild_projects(self):
         projects = []
         paginator = self.aws_codebuild_client.get_paginator('list_projects')
@@ -27,9 +28,9 @@ class Tester(interfaces.TesterInterface):
 
         for page in response_iterator:
             projects.extend(page['projects'])
-        
+
         return projects
-    
+
     def _append_codebuild_test_results(self, item, item_type, test_name, issue_status):
         return {
             "user": self.user_id,
@@ -41,26 +42,29 @@ class Tester(interfaces.TesterInterface):
             "test_name": test_name,
             "test_result": issue_status
         }
-    
+
     def codebuild_project_build_artifacts_should_be_encrypted(self):
         result = []
         test_name = "codebuild_project_build_output_artifacts_should_be_encrypted"
 
         projects = self.codebuild_projects
 
-        if len(projects) > 0:
+        if projects:
             response = self.aws_codebuild_client.batch_get_projects(names=projects)
             projects_details = response['projects']
-            projects_not_found = response['projectsNotFound']
 
             for project in projects_details:
                 project_arn = project['arn']
-                artifacts_encryption_disable = project['artifacts']['encryptionDisabled']
 
-                if artifacts_encryption_disable:
-                    result.append(self._append_codebuild_test_results(project_arn, "aws_codebuild_project", test_name, "issue_found"))
+                artifacts_encryption_disable = project['artifacts'].get('encryptionDisabled')
+
+                if artifacts_encryption_disable is not None:
+                    if artifacts_encryption_disable:
+                        result.append(self._append_codebuild_test_results(project_arn, "aws_codebuild_project", test_name, "issue_found"))
+                    else:
+                        result.append(self._append_codebuild_test_results(project_arn, "aws_codebuild_project", test_name, "no_issue_found"))
                 else:
                     result.append(self._append_codebuild_test_results(project_arn, "aws_codebuild_project", test_name, "no_issue_found"))
         else: pass
-       
+
         return result
