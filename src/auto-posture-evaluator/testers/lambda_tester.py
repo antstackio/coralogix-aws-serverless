d9b36d4176c0bfbaa48253ddbeb6027d1aeab20d
sleepy0owl <souravcristiano502@gmail.com>
2022-06-27 20:10:54 +0530

    Add region validation

--- src/auto-posture-evaluator/testers/lambda_tester.py
+++ src/auto-posture-evaluator/testers/lambda_tester.py
@@ -10,11 +10,12 @@ from concurrent.futures import ThreadPoolExecutor
 
 class Tester(interfaces.TesterInterface):
     def __init__(self, region_name) -> None:
+        self.aws_region = region_name
         self.aws_lambda_client = boto3.client('lambda', region_name=region_name)
         self.user_id = boto3.client('sts').get_caller_identity().get('UserId')
         self.account_arn = boto3.client('sts').get_caller_identity().get('Arn')
         self.account_id = boto3.client('sts').get_caller_identity().get('Account')
-        self.functions = self._get_all_functions()
+        self.functions = []
         self.SUPPORTED_LAMBDA_RUNTIME = "https://cgx-s3-nsm-logshipper-config.s3.eu-west-1.amazonaws.com/acceptable-lambda-runtime-versions.json"
 
     def declare_tested_service(self) -> str:
@@ -24,18 +25,33 @@ class Tester(interfaces.TesterInterface):
         return 'aws'
 
     def run_tests(self) -> list:
-        executor_list = []
-        return_values = []
+        all_regions = self._get_all_aws_regions()
 
-        with ThreadPoolExecutor() as executor:
-            executor_list.append(executor.submit(self.get_lambda_publicly_accessible))
-            executor_list.append(executor.submit(self.get_lambda_has_access_to_vpc_resources))
-            executor_list.append(executor.submit(self.get_lambda_uses_latest_runtime))
+        if any([self.aws_region == region for region in all_regions]):
+            executor_list = []
+            return_values = []
+            self.functions = self._get_all_functions()
+            with ThreadPoolExecutor() as executor:
+                executor_list.append(executor.submit(self.get_lambda_publicly_accessible))
+                executor_list.append(executor.submit(self.get_lambda_has_access_to_vpc_resources))
+                executor_list.append(executor.submit(self.get_lambda_uses_latest_runtime))
 
-            for future in executor_list:
-                return_values.extend(future.result())
+                for future in executor_list:
+                    return_values.extend(future.result())
 
-        return return_values
+            return return_values
+        else:
+            return None
+
+    def _get_all_aws_regions(self):
+        all_regions = []
+        boto3_client = boto3.client('ec2', region_name='us-east-1')
+        response = boto3_client.describe_regions(AllRegions=True)
+
+        for i in response['Regions']:
+            all_regions.append(i['RegionName'])
+
+        return all_regions
 
     def _get_all_functions(self) -> List:
         paginator = self.aws_lambda_client.get_paginator('list_functions')
